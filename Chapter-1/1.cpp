14ef2851cabc922353196b69047788f9254ae95a
Tejas Shah <tejasanilshah@gmail.com>
2017-07-29 23:05:37 +0530

    Improved code modularity + Added good question to ask

--- Chapter-1/1.cpp
+++ Chapter-1/1.cpp
@@ -5,6 +5,8 @@ Question 1.1: Implement an algorithm to determine if a string has all unique cha
 Variant: What if you cannot use additional data structures?
 -----
 Good questions to ask: What is the range of input characters? Depending on the input, the answer gets narrower or broader.
+If it is ASCII then 128 characters, extended ASCII 256
+You will simply initialize a boolean array of the same size and set the value to true the first time you encounter a character.
 */
 #include <cmath>
 #include <cstdio>
@@ -13,20 +15,29 @@ Good questions to ask: What is the range of input characters? Depending on the i
 #include <algorithm>
 #include <string>
 using namespace std;
+bool findDuplicateBB(const string& input);
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     string input;
     cout<<"Enter the input string: ";
     cin>>input;
+    bool foundDuplicate = findDuplicateBB(input);
+    if(foundDuplicate){
+        cout<<"Duplicate found"<<endl;
+    } else {
+        cout<<"No duplicates in the string"<<endl;
+    }
+    return 0;
+}
+bool findDuplicateBB(const string& input){
     int length = input.length();
     for(int i=0;i<length-1;i++){
         for(int j=i+1;j<length;j++){
             if(input[i]==input[j]){
-                cout<<"Duplicate found"<<endl;
-                return 0;
+                cout<<input[i]<<" "<<input[j]<<endl;
+                return true;
             }
         }
     }
-    cout<<"No duplicates in the string"<<endl;
-    return 0;
+    return false;
 }
